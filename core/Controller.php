797f96c64a35a8690b313c32452f5522cb7ee4f6
dilsoncampelo10 <dilsono316@gmail.com>
2023-07-30 16:34:16 -0300

    feat: create controller

---
+++ core/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace core;
+
+class Controller
+{
+    public function view(string $view, array $data = []): void
+    {
+        extract($data);
+        require_once "resources/views/" . $view . "php";
+    }
+
+    public function redirect(string $location): void
+    {
+        header("location: http://localhost/academy_library/" . $location);
+        exit();
+    }
+}
