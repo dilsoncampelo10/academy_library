77938a45d2eb5d544e0c5cffca015ce7a4f830c3
dilsoncampelo10 <dilsono316@gmail.com>
2023-07-30 21:19:48 -0300

    style: add bootstrap

--- resources/templates/main.php
+++ resources/templates/main.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Academy Library</title>
+    <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -20,6 +21,7 @@
         <p>Todos os direitos reservados</p>
     </footer>
 
+    <script src="public/assets/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
